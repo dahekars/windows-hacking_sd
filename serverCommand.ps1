d9e2d68349ea02e535029f3e2b9e8b715166341f
dahekars <dahekars@gmail.com>
2022-07-22 11:44:00 +0530

    adding server-side command file

---
+++ serverCommand.ps1
@@ -0,0 +1,10 @@
+
+#SConfig is a tools set shortcut
+
+#update NetWork ip to Static, Change Default Getway to *.*.*.2, DNS to IP of Server (for Domain Controler only), Change Server name   
+
+
+install-windowsfeature AD-Domain-Services -IncludemanagementTools
+# Install AD servies on server 
+
+
